836f4da5b82ca658468c701e9773750bf198a996
Adriano Canto Junior <adrianocanto462@gmail.com>
2026-04-23 12:34:11 -0300

    Documentado

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// declarando variaveis para usar o express e o socket.io de forma facilitada
 const express = require('express');
 const path = require('path');
 
@@ -5,26 +6,30 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'public'));
-app.engine('html', require('ejs').renderFile);
+// definindo onde o express vai procurar os arquivos .html .css entre outros
+app.use(express.static(path.join(__dirname, 'public'))); // localizando diretório public na raiz do sistema
+app.set('views', path.join(__dirname, 'public')); // direcionando as viwes para a pasta public
+app.engine('html', require('ejs').renderFile); // alterando a forma de renderização para reconhecer arquivos .html pois o padrão é .ejs
 app.set('view engine', 'html')
 
+// rota padrão renderizando o index.html
 app.use('/', (req, res) => {
     res.render('index.html')
 });
 
+// lista de mensagens do chat em um array. Obs: Não tem banco de dados para as mensagens e usuários por estava com prequisa :)
 let messages = []
 
+// socket.io e tratamento dos dados
 io.on('connection', socket => {
-    console.log(`Socket conectado: ${socket.id}`);
+    console.log(`Socket conectado: ${socket.id}`); // básico comando para saber se o socker.io está funcionando na pagina
 
-    socket.emit('previousMessages', messages);
+    socket.emit('previousMessages', messages); // mostrando as mensagens do array mesmo se atualziar a pagina 
 
     socket.on('sendMessage', data => {
-        messages.push(data);
-        socket.broadcast.emit('newMessage', data);
+        messages.push(data); // puxando as mesnagens para o atributo data
+        socket.broadcast.emit('newMessage', data); // mostrando novas mensagens na tela
     })
 });
 
-server.listen(3000);
+server.listen(3000); // porta do servidor
